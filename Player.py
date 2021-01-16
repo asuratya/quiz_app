caac8183979e7fc2e98eda630b14d2d73e4b430f
gabreey <gabriella_waffo@yahoo.com>
2021-01-16 19:35:28 +0100

    latest version

--- Player.py
+++ Player.py
@@ -3,12 +3,29 @@ class Player():
         def __init__(self, name):
                 # private attributes!
                 self._name = name
-                self._scores = [] 
+                self._score=0
+                self._finalScore = [] 
                 # store scores in list, overlaying QuizGames responsibility to keep track of corresponding category
-        def GetName(self): # was new in my version
+        def getName(self): # was new in my version
                 return self._name
+        
+        def getScore(self):
+            return self._score
+        
+        def addScore(self):
+            self._score = self._score + 100
+        
+        def subScore(self):
+            self._score = self._score - 50
+        
+        def updateFinalScore(self, my_score):
+            self._finalScore.append(my_score)
+            
+        def getFinalScore(self):
+            return self._finalScore[-1]
+            
                 
-        def AddScoresCategory(self):
+        '''def AddScoresCategory(self):
                 self._scores.append(0) # add a new category score that starts at 0
 
         def AddScore(self, index, amount): # add amount to scores[index]
@@ -16,4 +33,4 @@ class Player():
                         self._scores[index] = self._scores[index] + amount
 
         def __str__(self): # this could be made prettier
-                return self._name + ": \n \t" + self._scores.__str__()
+                return self._name + ": \n \t" + self._scores.__str__()'''
