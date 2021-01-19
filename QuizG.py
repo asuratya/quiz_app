886817a16681c76677a664a40d79e8dca215b849
gabreey <gabriella_waffo@yahoo.com>
2021-01-19 23:11:49 +0100

    small updates

---
+++ QuizG.py
@@ -0,0 +1,237 @@
+import tkinter as tk  # Tkinter library for GUI
+from tkinter import *
+import random
+import time
+from Player import *
+from QuizQuestion import *
+from tkinter.messagebox import showinfo
+import os
+class QuizG(): # mainly a class to host all user interface objects + some data (questions, players)
+    def __init__(self):
+
+        # setting up the root of the user interface = menuWindow
+        self._MenuWindow()
+
+        # initialize these as None or compiler will complain the class doesn't have these as attributes
+        self._nameTextField = None # need ref to this to get content & to destroy
+        self._nameButton = None # need ref to this to destroy
+        self._pWindow = None # need ref to this to destroy
+        self._mainWindow = None # need ref to this to add new Qframe
+        self._Qframe = None # put widgets of mainwindow inside a frame together for easy destroy
+
+        self._currentQ = None # current question being displayed/evaluated
+        self._currentP = None # current active player
+        self._currentcatindex = None # current category of questions
+        
+        # better would be: .txt files per topic with questions + function to add to game
+        self._topics = [] # fill with names of question topics
+        self._questions = [] # a nestedlist: contains lists with questions per topic
+
+        self._players = [] 
+        # fill with players in record.. maybe link to .txt file to keep player info even when closing game:
+        # creating game loads player file into _players and saving game overwrites player file with new info
+
+    def run(self): # starting window of user interface is launched
+        self._menuWindow.mainloop()
+
+    def _MenuWindow(self):
+        self._menuWindow = tk.Tk()
+        self._menuWindow.title("VUB QUIZ APP")
+        self._menuWindow.geometry('500x300')
+        self._menuWindow.resizable(0,0) # not resizable: change?
+        self._menuWindow.configure(bg = "black")
+
+        # start screen: 4 buttons: new player, select player, view stats, save game
+        # title at top of every window
+        title = tk.Label(self._menuWindow, text = 'VUB QUIZ APP', font = ('impact', 20, 'italic'), justify = tk.CENTER, bg = 'goldenrod2', fg = 'white')
+        title.pack(side = tk.TOP, fill = tk.BOTH)
+        
+        # text box
+        txt_box = tk.Label(self._menuWindow, text = "Are you ready to get your IQ tested?", font = "50", fg = 'white', bg = 'black')
+        txt_box.place(x = 100, y = 100)
+
+        # make these buttons prettier?:
+        newPButton = tk.Button(master=self._menuWindow, text="Create new player", fg='white',bg='goldenrod2',activebackground='gold',command = self._EnterName)
+        newPButton.place(x = 50, y = 140)
+
+        selectPButton = tk.Button(master=self._menuWindow, text="Select a player", fg='white' , bg='goldenrod2',command = self._SelectPWindow)
+        selectPButton.place(x = 50, y = 180)
+
+        viewStatsButton = tk.Button(master=self._menuWindow, text="View stats - under construction", fg='white' , bg='goldenrod2', command = None)
+        # should change command to show stats: still making this function
+        viewStatsButton.place(x = 50, y = 220)
+
+
+        saveButton = tk.Button(master=self._menuWindow, text="Save - under construction", command = None)
+        # should change command to write player info to .txt: still making this function
+        saveButton.place(x = 50, y = 260)     
+
+    def _EnterName(self): # called by "new player" button: open new entry and button on menuwindow
+
+        self._nameTextField = tk.Entry(master = self._menuWindow, text = "player name", width = 30)
+        self._nameTextField.place(x = 190, y = 140)
+
+        self._nameButton = tk.Button(master = self._menuWindow, text = "Confirm", command = self._AddPlayer)
+        self._nameButton.place(x = 400, y = 140)
+
+    def _AddPlayer(self): # called by Confirm button, create new player instance + put inside local list of players
+        name = self._nameTextField.get()
+        # should actually check if player already exists...!
+        p = Player(name)
+        self._players.append(p)
+        self._nameButton.destroy()
+        self._nameTextField.destroy()
+
+    def _SelectPWindow(self):
+        self._pWindow = tk.Toplevel(master = self._menuWindow) # new window
+        self._pWindow.geometry('500x300')
+        self._pWindow.resizable(0,0) # not resizable: change this?
+        self._pWindow.title('SELECT A PLAYER')
+        self._pWindow.config(bg ='black')
+        title = tk.Label(self._pWindow, text = 'VUB QUIZ APP', font = ('impact', 20, 'italic'), justify = tk.CENTER, bg = 'goldenrod2', fg = 'white')
+        title.pack(side = tk.TOP, fill = tk.BOTH)
+
+        # display all players as buttons
+        # dict to bind button and index: know which button was called = player index
+        buttondict = {}
+        index = 0
+        for player in self._players:
+            # command is a lambda function to pass input arg
+            b = tk.Button(master = self._pWindow, text = player.GetName(), command = lambda: self._playingWindow(buttondict[b]))
+            buttondict[b] = index 
+            b.pack(side = tk.TOP, fill = tk.X) # make buttons stretched over window width
+            index = index + 1
+        
+    def _playingWindow(self, index):
+        self._pWindow.destroy()
+        self._currentP = self._players[index]
+
+        self._mainWindow = tk.Toplevel(master = self._menuWindow) # new window for the main game part
+        self._mainWindow.geometry('500x300')
+        self._mainWindow.resizable(0,0) # not resizable: change this?
+        self._mainWindow.title('QUIZ')
+        self._mainWindow.config(bg ='black')
+
+        
+        title = tk.Label(self._mainWindow, text = 'VUB QUIZ APP', font = ('impact', 20, 'italic'), justify = tk.CENTER, bg = 'goldenrod2', fg = 'white')
+        title.pack(side = tk.TOP, fill = tk.BOTH)
+
+        self._UpdateQuestion(-1) # display the question
+    
+    def quit_game():
+        quit = showinfo(message="Congratulations you finish the game") 
+        if quit == 'ok':
+            sys.exit(menuWindow.destroy()) 
+        
+    def show_result(self):
+        global Name,score
+        self._resWindow = tk.Toplevel(master = self._menuWindow) # new window
+        top=self._resWindow
+        top.geometry('200x100')
+        top.resizable(0,0)
+        top.title('Quiz Result')
+        top.config(bg='blue')
+        top.protocol('WM_DELETE_WINDOW',quit_game)
+        filename= Name + 'txt'
+        data = '\nPlayer: ' + Name + '\nScore: ' + str(score)
+        with open(os.path.join(os.path.expanduser('~'),'results',filename),'a') as file:
+            filename.write(data)
+        lbl = Label(top, text='Quiz Over...\n Score: ' + str(score),font=30,fg='white',bg='blue').place(x=50,y=25)
+        exit_button = Button(top, text='Exit', width=10,bg='black', fg='red',command=quit_game).place(x=50,y=70)
+        
+        
+    
+    def _UpdateQuestion(self, prevAIndex):
+        # step 1: update player stats based on prev Q (if there was one)
+        
+        if (self._currentQ != None): # we had a prev Q
+            txt = ''
+            if (self._currentQ.IsCorrectA(prevAIndex)): # player answered correctly
+                self._currentP.AddScore(self._currentcatindex, 100)
+                txt = 'You were correct and earned 100 points.'
+
+            else: # player was wrong
+                self._currentP.AddScore(self._currentcatindex, -50) 
+                txt = 'You were wrong, the correct answer was ' + self._currentQ.getCorrectA() + '. You lost 50 points.'
+
+            # show message box saying (in)correct for a few seconds
+            resultbox = tk.Label(master = self._Qframe, text = txt, fg='green')
+            resultbox.pack() # this doesn't show up???
+
+            time.sleep(3) # making Python sleep to display this is not a great solution... replace?
+            self._Qframe.destroy()
+
+        self._Qframe = tk.Frame(self._mainWindow, bg = 'black') # encloses the other widgets so we can easily destroy them for the next Q
+        self._Qframe.pack(expand = True, fill = tk.BOTH) # frame just fills rest of whole window
+
+        # step 2: generating next Q:
+        # fetch a category based on self._currentP current stats (separate function?)
+        self._currentcatindex = 0 # for now for testing: take cat 0
+        self._currentQ = self._GetQuesion(self._currentcatindex) # quiz question to ask
+
+        # generate mainwindow questionbox
+        # make prettier
+        self._Qbox = tk.Label(master = self._Qframe, text = self._currentQ.getQ(), fg='green')
+        self._Qbox.pack()
+
+        # displaying possible answers: use same dictionary trick as with select player to know which button is clicked
+        buttondict = {}
+        index = 0
+        for item in self._currentQ.getA():
+            b = tk.Button(master = self._Qframe, text = item, command = lambda: self._UpdateQuestion(buttondict[b]))
+            # actually command should check if right, update player stats and fetch new playingWindow: still to be made
+            b.pack()
+            buttondict[b] = index
+            index = index + 1
+    
+    def _GetQuesion(self, topicindex): # getting a random question for a topic
+        index = random.randint(0,len( self._questions[topicindex] ) - 1)
+        return (self._questions[topicindex])[index]
+
+    # these functions should become private to use with reading the .txt files but still to be implemented
+
+    def AddQuestion(self, topic_index, question):
+        if ((topic_index < len(self._questions)) and (topic_index >= 0) ): # make sure topic_index is valid
+            self._questions[topic_index].append(question)
+
+    def AddTopic(self, topicname):
+        self._topics.append(topicname)
+        self._questions.append([])
+    
+     
+# testing: these are just some dumb examples to test for now
+#*******************************************************************
+game = QuizG()
+
+game.AddTopic("History")
+H_index = 0
+q1 = QuizQuestion("When did Belgium become independent?", ["1800", "1812", "1830"], 2)
+game.AddQuestion(H_index, q1)
+
+game.AddTopic("Science")
+Sc_index = 1
+q2 = QuizQuestion("What is the first element in the periodic table?", ["Lithium", "Hydrogen", "Uranium"], 1)
+game.AddQuestion(Sc_index, q2)
+
+game.AddTopic("Sport")
+Sp_index = 2
+q3 = QuizQuestion("Who won the world championship soccer in 2018?", ["France", "Kroatia", "Germany"], 0)
+game.AddQuestion(Sp_index, q3)
+
+q4 = QuizQuestion('q2?',['a','b','c'],0)
+game.AddQuestion(H_index, q4)
+
+p1 = Player("P1")
+p2 = Player("P2")
+
+p1.AddScoresCategory() #history
+p1.AddScoresCategory() #science
+p1.AddScoresCategory() # sports
+
+p2.AddScoresCategory() #history
+p2.AddScoresCategory() #science
+p2.AddScoresCategory() # sports
+
+game._players = [p1, p2]
+
+game.run()
--- quiz.py
+++ quiz.py
@@ -1,11 +1,286 @@
 #####import some libraries
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+from Player import Player
 import random,sys
-from tkinter.ttk import Separator,Progressbar
-from tkinter.messagebox import showinfo
 import os
+from QuizQuestion import QuizQuestion
+import pickle
+#from tkinter.ttk import Separator,Progressbar
+#from tkinter.messagebox import showinfo
 
-##############to make the main window#############
+class QuizGame():
+# mainly a class to host all user interface objects + some data (questions, players)
+    def __init__(self):
+
+        # setting up the root of the user interface = menuWindow
+        self._MenuWindow()
+
+        # initialize these as None or compiler will complain the class doesn't have these as attributes
+        self._nameTextField = None  # need ref to this to get content & to destroy
+        self._nameButton = None  # need ref to this to destroy
+        self._pWindow = None  # need ref to this to destroy
+        self._mainWindow = None  # need ref to this to add new Qframe
+        self._Qframe = None  # put widgets of mainwindow inside a frame together for easy destroy
+
+        self._players = []
+        # fill with players in record.. maybe link to .txt file to keep player info even when closing game:
+        # creating game loads player file into _players and saving game overwrites player file with new info
+
+    def run(self):  # starting window of user interface is launched
+        self._menuWindow.mainloop()
+
+    def _MenuWindow(self):
+        self._menuWindow = tk.Tk()
+        self._menuWindow.title("VUB QUIZ APP")
+        self._menuWindow.geometry('500x300') #600x400
+        self._menuWindow.resizable(0, 0)  # not resizable: change?
+        self._menuWindow.configure(bg="black")
+
+        # start screen: 4 buttons: new player, select player, view stats, save game
+        # title at top of every window
+        title = tk.Label(self._menuWindow, text='VUB QUIZ APP', font=(
+            'impact', 20, 'italic'), justify=tk.CENTER, bg='dark slate blue', fg='white', name='app_title', height=2)
+        title.pack(side=tk.TOP, fill=tk.BOTH)
+
+        # make these buttons prettier?:
+        newPButton = tk.Button(master=self._menuWindow,
+                               text="Create new player", command=self._EnterName, width = 30,bg='slate blue',fg='black',activebackground='light grey',height=2 )
+        newPButton.place(x=130, y=120)
+               
+        viewStatsButton = tk.Button(master=self._menuWindow,
+                                    text="View stats",command=self._viewStat, width = 30,bg='slate blue',fg='black',activebackground='light grey',height=2)
+        # should change command to show stats: still making this function
+        viewStatsButton.place(x=130, y=190)
+
+
+    #this fxn destroys all the widgets except the app_title widget in the frame
+    def _destroyFrame(self):
+        for widget in self._menuWindow.winfo_children(): #travers through all the widget in the frme
+            if str(widget) != '.app_title':
+                widget.destroy()
+                
+    
+    def _EnterName(self):  # called by "new player" button: open new entry and button on menuwindow
+        self._destroyFrame()
+        self._txt_box = tk.Label(
+            self._menuWindow, text="Enter your Name: ", font="30",fg='white',bg='black')
+        self._txt_box.place(x=30, y=130)
+        self._nameTextField = tk.Entry(
+            self._menuWindow, text="player name", width=40)
+        self._nameTextField.place(x=200, y=133)
+
+        self._nameButton = tk.Button(
+            self._menuWindow, text="Confirm", command=self._AddPlayer, width=20, bg='slate blue',fg='black',activebackground='light grey' ) #calls the AddPlayer fxn when buttons is clicked
+        self._nameButton.place(x=200, y=220)
+
+    def _AddPlayer(self):  # called by Confirm button, create new player instance + put inside local list of players
+        name = self._nameTextField.get()
+        if len(name) == 0:
+            messagebox.showinfo("Error", "Enter your Name") #make sure the name field is not empty
+        else:
+            p = Player(name)
+            self._players.append(p)
+            self._startPlaying() #start the quiz game
+    #view the stats of the player in the    
+    def _viewStat(self):
+        self._statWindow = tk.Toplevel(master = self._menuWindow) # new window
+        self._statWindow.geometry('500x300')
+        self._statWindow.resizable(0,0) # not resizable: change this?
+        self._statWindow.title('Stats')
+        self._statWindow.config(bg ='black')
+        title = tk.Label(self._statWindow,text='VUB QUIZ APP', font=(
+            'impact', 20, 'italic'), justify=tk.CENTER, bg='dark slate blue', fg='white', name='app_title', height=2)
+        title.pack(side = tk.TOP, fill = tk.BOTH)
+        #load the picked list of players saved locally
+        players_list = self._loadPlayers()
+        for players in players_list: #traverse through the list of players object and create button for each
+            tk.Button(self._statWindow,
+                      text=players.getName(),
+                      width = 60,
+                      bg='black',
+                      fg='slate blue',
+                      activebackground='light grey',
+                      command=lambda players=players: self._viewScore(players)).pack(padx=5, pady=5)
+            
+    #show the score of the players in a message box when the button is clicked from the list of players    
+    def _viewScore(self, players):
+        score = players.getFinalScore()
+        name = players.getName()
+        msg = 'Score of ' + name + ' is: ' + str(score)
+        messagebox.showinfo('Score', msg)
+
+    #this fxn loads the pickled players and passes the objects as a list            
+    def _loadPlayers(self):
+        prev_cwd = os.getcwd()
+        os.chdir('players_data')
+        cwd = os.getcwd()
+        players = []
+        for files in os.listdir(cwd):
+            with open(files, 'rb') as openfile:
+                players.append(pickle.load(openfile))
+        os.chdir(prev_cwd)
+        return players
+        
+        
+
+    #this fxn shows the starting window for the quiz game
+    def _startPlaying(self):
+        self._destroyFrame()
+
+        txt_box = tk.Label(self._menuWindow,
+                           text="Are you ready to get your IQ tested " +
+                               self._players[0].getName() + "?",
+                           font="30", fg='white', bg='black')
+        txt_box.place(x=90, y=130)
+
+        b = tk.Button(master=self._menuWindow, text='Start Quiz', width=20, bg='slate blue',fg='black',activebackground='light grey',
+                      command=lambda: self._playingWindow())
+        
+        b.place(x=170, y=220)
+    
+    #this fxn displays the final window to the player with the score after completing the quiz
+    def _finalWindow(self): 
+        #show the points in the message box
+        def showPoints():
+            ltst_score = self._players[0].getScore()
+            self._players[0].updateFinalScore(ltst_score)
+            result = "Your final point is " + str(self._players[0].getFinalScore())
+            savePlayerInfo()
+
+            messagebox.showinfo("Result", result ) 
+           
+         
+        
+  
+        #save the info of the current player as a pickle object locally
+        def savePlayerInfo():
+            player_name = self._players[0].getName()
+            prev_dir=os.getcwd()
+            filename = player_name+ '.pk1'
+            os.chdir('players_data')
+            with open(filename, 'wb') as output:
+                pickle.dump(self._players[0], output, pickle.HIGHEST_PROTOCOL)
+            os.chdir(prev_dir)
+            
+        self._destroyFrame()
+        exit_label = tk.Label(self._menuWindow,
+                      text= 'Congratulations you have finished your quiz!!!' , font='10', fg='white', bg='black')
+        exit_label.place(x=80, y=110)
+        
+        btnShowAnswer = tk.Button(master=self._menuWindow, text='View Score', width=20, bg='dark slate blue',fg='black',activebackground='light grey',
+                      command= showPoints)
+        btnShowAnswer.place(x=180, y=200)
+
+        
+        
+    #shows the playing window i.e the frame where the questions are loaded
+    def _playingWindow(self):        
+        self._destroyFrame()
+        no_of_ques = 5
+        #random index of the question to show
+        index = randomQuestions(no_of_ques, (len(quiz_questions) -1))
+        for i in range(no_of_ques):
+            question, index = getQuestions(i, index)
+            self._showQuestions(question, index, i)
+            if i == (no_of_ques - 1): #check if the current question is the final question
+                self._finalWindow()       
+                    
+    #show the question and thei options one at a time through the for loop in the playingWindow function       
+    def _showQuestions(self, question, index, i):
+        q_label = str(i + 1) +'. ' + question
+        question_label = tk.Label(self._menuWindow,
+                      text= q_label , font='10', fg='white', bg='black')
+        question_label.place(x=100, y=80)
+        
+        answers = getAnswersOnTheList(index, i) #get answer of the current question
+        
+        #gets the value from the radio button selected and checks
+        def checkAnswer():
+            if radioVar.get() == '-1' : #checks if radio button is clicked or not
+                messagebox.showinfo("Error", 'Choose a option' )
+            else:
+                selection = radioVar.get()
+                correct_ans = getCorrectAnswer(index, i)
+                if selection == correct_ans:
+                    self._players[0].addScore()
+                else:
+                    self._players[0].subScore()
+                var2.set(1)                
+           
+        radioVar = tk.StringVar()
+        radioVar.set(-1)
+        
+        #initialized a variable for python to wait until it's value is changed
+        var2 = tk.IntVar()
+        
+        
+        R1 = tk.Radiobutton(self._menuWindow, text= answers[0],variable= radioVar, indicatoron=0, value = answers[0], width = 20, height=2, fg='black',bg='slate blue', activebackground='light grey')
+        R1.place(x=50, y=110) 
+      
+        R2 = tk.Radiobutton(self._menuWindow, text= answers[1], variable= radioVar,indicatoron=0, value = answers[1], width = 20,fg='black',height=2,bg='slate blue', activebackground='light grey')
+        R2.place(x=300, y=110)
+        
+        R3 = tk.Radiobutton(self._menuWindow, text= answers[2], variable= radioVar,indicatoron=0, value = answers[2], width = 20,fg='black',height=2,bg='slate blue', activebackground='light grey')
+        R3.place(x=50, y=200)
+        
+        R4 = tk.Radiobutton(self._menuWindow, text= answers[3], variable= radioVar,indicatoron=0, value = answers[3], width = 20,fg='black',height=2,bg='slate blue', activebackground='light grey')
+        R4.place(x=300, y=200)
+        
+        
+        next_button =  tk.Button(self._menuWindow, text = 'Next Question', command =checkAnswer,fg='black',bg='dark slate blue',activebackground='light grey')
+        next_button.place(x=210, y = 260)
+        
+        next_button.wait_variable(var2)
+        
+        
+
+
+
+# *******************************************************************
+questions_obj = []
+
+
+game = QuizGame()
+
+quiz_questions = [
+    ['Question No. 1:', '1', ['1', '2', '3', '4']],
+    ['Question No. 2:', '2', ['1', '2', '3', '4']],
+    ['Question No. 3:', '3', ['1', '2', '3', '4']],
+    ['Question No. 4:', '4', ['1', '2', '3', '4']],
+    ['Question No. 5:', '5', ['1', '5', '3', '4']],
+    ['Question No. 6:', '6', ['5', '2', '6', '4']],
+    ['Question No. 7:', '7', ['4', '8', '3', '7']],
+    ['Question No. 8:', '8', ['8', '7', '9', '4']],
+    ['Question No. 9:', '9', ['7', '9', '3', '0']]
+    ]
+
+#create questions objects and initialize them with the value from the list
+for items in quiz_questions:
+    questions_obj.append(QuizQuestion(items[0], items[1], items[2]))
+ 
+def getQuestions(i, index): 
+    return questions_obj[index[i]].getQuestion(), index
+
+def getAnswersOnTheList(index, i):
+    return questions_obj[index[i]].getAnswersList()
+
+def getCorrectAnswer(index, i):
+    return questions_obj[index[i]].getAnswer()
+
+def randomQuestions(num, end, start=0):
+    que_index = []
+
+    while(len(que_index) < num):
+        tmp = random.randint(start, end)
+        if tmp not in que_index:
+            que_index.append(tmp)
+    return que_index
+
+game.run()
+
+
+'''##############to make the main window#############
 root = Tk()
 root.title("VUB QUIZ APP")
 root.geometry('500x300')
@@ -59,7 +334,7 @@ def enable_allbuttons():
           
 ######################## show the final result#########################
 def result():
-    global score,Name
+    global score,Name,Roll
     root.withdraw()
     top = Toplevel(root)
     top.geometry('200x100')
@@ -169,4 +444,4 @@ submit.pack(side=BOTTOM)
 if __name__ =="__main__":
     root.withdraw()
     addwidget()
-    root.mainloop()
+    root.mainloop()'''
